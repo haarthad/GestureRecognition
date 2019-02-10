7c0fdcd1bd4e96644473a1a2494e53c9227de8eb
KrollTroll <31713317+KrollTroll@users.noreply.github.com>
2019-02-09 18:55:59 -0600

    Revert "Refined CalendarGrabber.py to have more readable output"
    This reverts commit 5e8e775150c957ef9e0f8a886e59e2fadf6e1ca0.

--- Python Subsystem/DeviceCommands/CalendarGrabber.py
+++ Python Subsystem/DeviceCommands/CalendarGrabber.py
@@ -3,21 +3,31 @@ import datetime
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import logging
+
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 
-def main():
+def main(commands_path):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
+
+    # Ignore discovery cache warnings as we are using a version
+    # of oauth2client >= 4.0.0
+    logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
+    # Ignore URL request prints
+    logging.getLogger('googleapiclient.discovery').setLevel(logging.ERROR)
+
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    store = file.Storage('token.json')
+    store = file.Storage(commands_path + "token.json")
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        flow = client.flow_from_clientsecrets(commands_path + "credentials.json", SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
 
@@ -28,18 +38,12 @@ def main():
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
+
     if not events:
         print('No upcoming events found.')
     for event in events:
-        startInfo = event['start'].get('dateTime', event['start'].get('date'))
-        endInfo = event['end'].get('dateTime', event['end'].get('date'))
-        day = startInfo.split('T')[0]
-        if 'T' in startInfo:
-            startTime = (startInfo.split('T')[1]).split('-')[0]
-            endTime = (endInfo.split('T')[1]).split('-')[0]
-            print(day, event['summary'], startTime, '-', endTime)
-        else:
-            print(day, event['summary'])
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
 
 if __name__ == '__main__':
-    main()
+    main("")
